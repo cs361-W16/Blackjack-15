8660d85ccd2ebfd2855a9022d2496cb0a77af5d8
Nic Desilets <nic@desilets.org>
2016-03-12 23:20:27 -0800

    Theming and card offset classes

--- src/main/java/assets/js/app.js
+++ src/main/java/assets/js/app.js
@@ -1,6 +1,25 @@
 angular.module('Blackjack', ['ngMaterial', 'ngAnimate'])
     .config(function($mdThemingProvider){
+        var greenMap = $mdThemingProvider.extendPalette('green', {
+            '100': '1abc9c',
+            '300': '1abc9c',
+            '500': '1abc9c',
+            '700': '1abc9c',
+            '900': '1abc9c'
+        });
+
+        var darkGreenMap = $mdThemingProvider.extendPalette('green', {
+            '100': '16a085',
+            '300': '16a085',
+            '500': '16a085',
+            '700': '16a085',
+            '900': '16a085'
+        });
+
+        $mdThemingProvider.definePalette('greenMap', greenMap);
+        $mdThemingProvider.definePalette('darkGreenMap', darkGreenMap);
+
         $mdThemingProvider.theme('default')
-            .primaryPalette('green')
-            .accentPalette('red');
+            .primaryPalette('greenMap')
+            .accentPalette('darkGreenMap');
     });
